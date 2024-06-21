99397bfb2b1ec6f3badcd2f9cf5a8ce96a4af62d
Rylan Graham <95762889+RylanJGraham@users.noreply.github.com>
2024-06-21 17:06:28 +0200

    Form Fix, CV Added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -41,6 +41,7 @@ const Contact = () => {
                 <div className="flex flex-col xl:flex-row gap-[30px]">
                     <div className="xl:w-[54%] order-2 xl:order-none">
                         {/* form */} 
+                        <p className="text-accent ">*Form is under development. Please send me an email or a message to the information provided here. Thanks!</p>
                         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                             <h3 className="text-4xl text-accent">Let's Work Together!</h3>
                             <p className="text-white/60"></p>
@@ -72,6 +73,7 @@ const Contact = () => {
                             {/* btn */}
                             <Button size="md" className="max-w-40">Send Message</Button>
                         </form>
+                        
                     </div>
                     <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
                         {/* info */} 
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
 import {FiDownload} from "react-icons/fi";
 
+// Replace 'cv.pdf' with the actual filename of your CV
+const cvFileUrl = '/assets/pdf/RylanJamesGrahamResume.pdf';
+
 //components
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
@@ -19,12 +22,14 @@ const Home = () => {
             <p className="max-w-[500px] mb-9 text-white/80">I excel at creating engaging Videogames, elegant Mobile/Web software experiences, beautiful 3D digital artistry, and I am proficient in various programming languages & softwares</p>
             {/* btn & socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+              <a href={cvFileUrl} download="Rylan_Graham_CV.pdf" className="uppercase flex items-center gap-2">
+                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
                 <span>Download CV</span>
-                <FiDownload className="text=xl"/>
-              </Button>
+                <FiDownload className="text-xl"/>
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
-                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center 
+              <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center 
                 items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
               </div>
             </div>
