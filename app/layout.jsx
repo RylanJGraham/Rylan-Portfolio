f9751ebb254949bbe2ac5e04761314061aceddad
rylangraham02 <rylangraham02@gmail.com>
2025-01-21 12:04:06 +0000

    SpeedInsights

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // components
 import Header from "@/components/Header";
